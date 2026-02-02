e48fec5239efe5ef67cafbc98fb20e83deee54b1
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2026-02-02 18:17:41 +0600

    implement parallax effect

--- src/sections/Parallax.jsx
+++ src/sections/Parallax.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Chocolate from "../assets/images/choclate-bg-2.png"
 const Parallax = () => {
   return (
-    <div>Parallax</div>
+    <section className='inner-container h-screen py-20'>
+      <div className='h-full rounded-xl bg-fixed bg-center bg-cover' style={{backgroundImage:`url(${Chocolate})`}}></div>
+    </section>
   )
 }
 
